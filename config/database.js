955ff5acfe345c5d8d4f1e13d439f75023341558
Risheek <risheek2627.com>
2025-01-25 14:45:25 +0530

    crted database name

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: "localhost",
   user: "root",
   password: "rayaru",
-  database: "task_traker",
+  database: "task_tracker",
 });
 
 module.exports = pool.promise();
