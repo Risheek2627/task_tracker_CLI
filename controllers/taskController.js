15efaef18d9b241ebf0f351cded2cacfc6e48a68
Risheek <risheek2627.com>
2025-01-25 13:37:17 +0530

    added in-progress , done status and listing the tasks with details

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -40,3 +40,67 @@ const deleteTask = async (id) => {
     console.log("Error deleting task :", error.message);
   }
 };
+
+//mark a task as in-progress
+const markInProgress = async (id) => {
+  try {
+    const query =
+      "UPDATE tasks SET status = 'in-progress' , updateAT = NOW() WHERE id = ? ";
+    const [result] = await db.query(query, [id]);
+    if (result.affectedRows > 0) {
+      console.log(`Task marked as in-progress (ID : ${id})`);
+    } else {
+      console.log(`Task with id ${id} not found`);
+    }
+  } catch (error) {
+    console.log("Error marking in-progress", error.message);
+  }
+};
+
+//mark a task as done
+const markDone = async (id) => {
+  try {
+    const query =
+      "UPDATE tasks SET status = 'done' , updateAT = NOW() WHERE id = ? ";
+    const [result] = await db.query(query, [id]);
+    if (result.affectedRows > 0) {
+      console.log(`Task marked as done (ID : ${id})`);
+    } else {
+      console.log(`Task with id ${id} not found`);
+    }
+  } catch (error) {
+    console.log("Error in mark as done : ", error.message);
+  }
+};
+
+// list all tasks (with optional status )
+
+const listTasks = async (status = null) => {
+  try {
+    const query = "SELECT * FROM tasks";
+    if (status) {
+      query + "WHERE status=?";
+    }
+    const [result] = await db.query(query, [status]);
+    if (result.affectedRows === 0) {
+      console.log("No tasks found");
+    } else {
+      result.forEach((task) => {
+        console.log(
+          `ID : ${task.id}, description : ${task.description} , status : ${task.description}`
+        );
+      });
+    }
+  } catch (error) {
+    console.log("Error listing the tasks", error.message);
+  }
+};
+
+module.exports = {
+  addTask,
+  updateTask,
+  deleteTask,
+  markInProgress,
+  markDone,
+  listTasks,
+};
