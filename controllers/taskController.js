abdb9b8b85d8be23c5ef0518aaab879854f5239d
Risheek <risheek2627.com>
2025-01-25 13:19:28 +0530

    added code to update and delete the task

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -10,3 +10,33 @@ const addTask = async (description) => {
     console.log(`Error adding task`, error.message);
   }
 };
+
+// update the task
+const updateTask = async (id, description) => {
+  try {
+    const query = "UPDATE tasks SET description=?, updateAt= NOW() WHERE id=?";
+    const [result] = await db.query(query, [description, id]);
+    if (result.affectedRows > 0) {
+      console.log(`Task updated successfully (ID : ${id})`);
+    } else {
+      console.log(`Task with ID ${id} not found`);
+    }
+  } catch (error) {
+    console.log("Error updating the task", error.message);
+  }
+};
+
+// delete the task
+const deleteTask = async (id) => {
+  try {
+    const query = "DELETE FROM tasks WHERE id= ?";
+    const [result] = await db.query(query, [id]);
+    if (result.affectedRows > 0) {
+      console.log(`task deleted successfully (ID : ${id})`);
+    } else {
+      console.log(`Task with ID ${id} not found`);
+    }
+  } catch (error) {
+    console.log("Error deleting task :", error.message);
+  }
+};
